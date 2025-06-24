99108fa720e724f57b5277d28e3902ca3b95d122
ddbrendan <119946469+ddbrendan@users.noreply.github.com>
2025-06-24 15:39:41 +0200

    fix test errors

--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -2,22 +2,27 @@ require "test_helper"
 
 class EntriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get entries_index_url
+    get entries_url
     assert_response :success
   end
 
   test "should get new" do
-    get entries_new_url
+    get new_entry_url
     assert_response :success
   end
 
-  test "should get create" do
-    get entries_create_url
-    assert_response :success
+  test "should create entry" do
+    assert_difference('Entry.count') do
+      post entries_url, params: { entry: { food_name: "Test Food", grams: 100.0, date: Date.current, custom_calories_per_100g: 100.0, custom_carbs_per_100g: 10.0, custom_protein_per_100g: 5.0, custom_fats_per_100g: 2.0 } }
+    end
+    assert_redirected_to root_path
   end
 
-  test "should get destroy" do
-    get entries_destroy_url
-    assert_response :success
+  test "should destroy entry" do
+    entry = entries(:one)
+    assert_difference('Entry.count', -1) do
+      delete entry_url(entry)
+    end
+    assert_redirected_to root_path
   end
 end
