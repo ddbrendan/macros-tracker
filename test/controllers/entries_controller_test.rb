f30c386a4b84bfa21fa105db510aeb91302d728c
ddbrendan <119946469+ddbrendan@users.noreply.github.com>
2025-06-24 15:43:18 +0200

    fix rubocop errors

--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -12,7 +12,7 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create entry" do
-    assert_difference('Entry.count') do
+    assert_difference("Entry.count") do
       post entries_url, params: { entry: { food_name: "Test Food", grams: 100.0, date: Date.current, custom_calories_per_100g: 100.0, custom_carbs_per_100g: 10.0, custom_protein_per_100g: 5.0, custom_fats_per_100g: 2.0 } }
     end
     assert_redirected_to root_path
@@ -20,7 +20,7 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
 
   test "should destroy entry" do
     entry = entries(:one)
-    assert_difference('Entry.count', -1) do
+    assert_difference("Entry.count", -1) do
       delete entry_url(entry)
     end
     assert_redirected_to root_path
